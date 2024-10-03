5bc03bc6c2fc0cb1ed3abeb3ba97f090b3cdbabd
AntonioPrata <antonio.prata1@yahoo.com.br>
2024-10-03 15:07:03 -0300

    Criando classes no Admin

--- sistema_patinhas/aplic/admin.py
+++ sistema_patinhas/aplic/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from aplic.models import Animal
+from aplic.models import Tutor
 
+admin.site.register(Animal)
+admin.site.register(Tutor)
 # Register your models here.
--- sistema_patinhas/aplic/models.py
+++ sistema_patinhas/aplic/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Animal(models.Model):
+    nome = models.CharField(max_length=50)
+    peso = models.FloatField()
+    data_nascimento = models.DateField
+    raca = models.CharField(max_length=25)
+    class Meta:
+        verbose_name_plural = 'Animais'
+
+    def __str__(self):
+        
+        return self.nome
+    
+class Tutor(models.Model):
+    CPF = models.FloatField(10)
+    nome = models.CharField(max_length=50)
+    data_nascimento = models.DateField
+    class Meta:
+        verbose_name_plural = 'Tutores'
+    
+    def __str__(self):
+        
+        return self.nome
+
+
+
