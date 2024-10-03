d2e65a8305311a944c9e4ecf9f327883c0930b6b
AntonioPrata <antonio.prata1@yahoo.com.br>
2024-10-03 19:21:11 -0300

    Criando classe tutor

--- sistema_patinhas/aplic/models.py
+++ sistema_patinhas/aplic/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Animal(models.Model):
     nome = models.CharField(max_length=50)
-    peso = models.FloatField()
+    peso = models.DecimalField
     data_nascimento = models.DateField
     raca = models.CharField(max_length=25)
+
     class Meta:
         verbose_name_plural = 'Animais'
 
@@ -13,15 +14,18 @@ class Animal(models.Model):
         return self.nome
     
 class Tutor(models.Model):
-    CPF = models.FloatField(10)
     nome = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=11, default='00000000000')
     data_nascimento = models.DateField
+
     class Meta:
         verbose_name_plural = 'Tutores'
     
     def __str__(self):
         
         return self.nome
+
+    
 
 
 
