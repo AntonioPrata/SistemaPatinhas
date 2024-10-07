d02d60001bf39bcf69cee85e042522c219103cb8
AntonioPrata <antonio.prata1@yahoo.com.br>
2024-10-07 20:30:15 -0300

    Atualizando classes e corrigindo erros

--- sistema_patinhas/aplic/admin.py
+++ sistema_patinhas/aplic/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from aplic.models import Animal
+from aplic.models import Gato
+from aplic.models import Cachorro
 from aplic.models import Tutor
+from aplic.models import Raca
 
-admin.site.register(Animal)
+
+admin.site.register(Cachorro)
+admin.site.register(Gato)
 admin.site.register(Tutor)
+admin.site.register(Raca)
+
 # Register your models here.
--- sistema_patinhas/aplic/models.py
+++ sistema_patinhas/aplic/models.py
@@ -1,13 +1,34 @@
+
 from django.db import models
+
+class SexOptions(models.TextChoices):
+    FEMALE = 'F', 'Femêa'
+    MALE = 'M', 'Macho'
 
 class Animal(models.Model):
     nome = models.CharField(max_length=50)
     peso = models.FloatField(('peso'), blank=True, null=True, max_length="5")
-    data_nascimento = models.DateField
-    raca = models.CharField(max_length=25)
+    data_nascimento = models.DateField(max_length=8 ,blank=True, null=True)
+    sexo = models.CharField(('sexo'), max_length=1, choices=SexOptions.choices)
+    personalidade = models.CharField()
+
+class Raca(models.Model):
+    nome_raca = models.CharField(max_length=50)
+    descricao_raca = models.TextField()
+
+class Cachorro(Animal):
+    porte = models.CharField(max_length=50)
+    raca = models.ForeignKey(Raca, on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name_plural = 'Animais'
+        verbose_name_plural = 'Cachorros'
+
+    def __str__(self):
+        return self.nome
+
+class Gato(Animal):
+    class Meta:
+        verbose_name_plural = 'Gatos'
 
     def __str__(self):
         
@@ -16,7 +37,9 @@ class Animal(models.Model):
 class Tutor(models.Model):
     nome = models.CharField(max_length=50)
     cpf = models.CharField(('CPF'),max_length=11, blank=True, null=True)
-    data_nascimento = models.DateField
+    data_nascimento = models.DateField(max_length=8 ,blank=True, null=True)
+    telefone = models.CharField(('Telefone'), max_length=11, blank=True, null=True)
+    email = models.EmailField(('Email'), blank=True, null=True)
 
     class Meta:
         verbose_name_plural = 'Tutores'
