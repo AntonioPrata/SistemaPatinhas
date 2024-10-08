ce7e4e6279c81133146426f55c69065ee2664882
iiszbella <isabellalouzado@gmail.com>
2024-10-07 23:36:34 -0300

    Atualização de classe e correção de erro

--- sistema_patinhas/aplic/models.py
+++ sistema_patinhas/aplic/models.py
@@ -1,54 +1,76 @@
-
 from django.db import models
+
 
 class SexOptions(models.TextChoices):
     FEMALE = 'F', 'Femêa'
     MALE = 'M', 'Macho'
 
+
+class Raca(models.Model):
+    nome_raca = models.CharField(("Raça"), max_length=50, blank=True, null=False)
+    descricao_raca = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'Raças'
+
+    def str(self):
+        return self.nome_raca
+
+
 class Animal(models.Model):
     nome = models.CharField(max_length=50)
     peso = models.FloatField(('peso'), blank=True, null=True, max_length="5")
-    data_nascimento = models.DateField(max_length=8 ,blank=True, null=True)
+    data_nascimento = models.DateField(max_length=8, blank=True, null=True)
     sexo = models.CharField(('sexo'), max_length=1, choices=SexOptions.choices)
     personalidade = models.CharField()
-
-class Raca(models.Model):
-    nome_raca = models.CharField(max_length=50)
-    descricao_raca = models.TextField()
+    raca = models.ForeignKey(Raca, on_delete=models.CASCADE, null=True)
 
 class Cachorro(Animal):
     porte = models.CharField(max_length=50)
-    raca = models.ForeignKey(Raca, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = 'Cachorros'
 
-    def __str__(self):
+    def str(self):
         return self.nome
+
 
 class Gato(Animal):
     class Meta:
         verbose_name_plural = 'Gatos'
 
-    def __str__(self):
-        
+    def str(self):
         return self.nome
-    
+
+
 class Tutor(models.Model):
     nome = models.CharField(max_length=50)
-    cpf = models.CharField(('CPF'),max_length=11, blank=True, null=True)
-    data_nascimento = models.DateField(max_length=8 ,blank=True, null=True)
+    cpf = models.CharField(('CPF'), max_length=11, blank=True, null=True)
+    data_nascimento = models.DateField(max_length=8, blank=True, null=True)
     telefone = models.CharField(('Telefone'), max_length=11, blank=True, null=True)
     email = models.EmailField(('Email'), blank=True, null=True)
 
     class Meta:
         verbose_name_plural = 'Tutores'
-    
-    def __str__(self):
-        
+
+    def str(self):
         return self.nome
 
-    
+class Tipo(models.Model):
+    nome_tipo = models.CharField(("Tipo"), max_length=50)
+    descricao_tipo = models.TextField('Descrição')
 
+    class Meta:
+        verbose_name_plural = 'Tipos'
 
+    def str(self):
+        return self.nome_tipo
 
+class Evento(models.Model):
+    data_inicio = models.DateField(('Data Início'), max_length=8, blank=True, null=True)
+    data_fim = models.DateField(('Data Fim'),max_length=8, blank=True, null=True)
+    descricao = models.CharField(('Descrição do Evento'), max_length=50)
+    tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Eventos'
